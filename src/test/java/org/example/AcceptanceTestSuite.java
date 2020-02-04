23006204d0d8f7dcdedd0470225bb00f7f2f4649
sszemer <Sk@te123#>
2020-02-04 12:07:45 +0100

    uruchamianie pojedyczego story

--- src/test/java/org/example/AcceptanceTestSuite.java
+++ src/test/java/org/example/AcceptanceTestSuite.java
@@ -1,5 +1,8 @@
 package org.example;
 
 import net.serenitybdd.jbehave.SerenityStories;
+import net.serenitybdd.jbehave.SerenityStories;
+import net.serenitybdd.jbehave.annotations.Metafilter;
 
+@Metafilter("+nbp")
 public class AcceptanceTestSuite extends SerenityStories {}
