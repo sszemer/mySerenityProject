f922e996faf4ae22fb16a96158ede85f3f7ba828
sszemer <Sk@te123#>
2020-02-05 09:46:37 +0100

    -deserializacja generyczna -asercja bezposrednio na body

--- src/test/java/org/example/steps/serenity/RESTSteps.java
+++ src/test/java/org/example/steps/serenity/RESTSteps.java
@@ -1,12 +1,15 @@
 package org.example.steps.serenity;
 
 import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.mapper.ObjectMapper;
 import net.thucydides.core.annotations.Step;
 import org.example.pojos.Nbp;
 import org.junit.Assert;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.config.JsonConfig.jsonConfig;
@@ -27,14 +30,18 @@ public class RESTSteps {
         rate = given().config(RestAssured.config().jsonConfig(jsonConfig().numberReturnType(BIG_DECIMAL)))
                 .and().get(address)
                 .then().statusCode(200).extract().jsonPath().get("[0].rates.find {it.code='" + currency + "'}.mid");
-
+//        deserializacja
         response = get(address).body().as(Nbp[].class);
         System.err.println(response[0].getTable());
+//        deserializaca generyczna
+        List<Map<String,Object>> genericResponse = get(address).as(new TypeRef<List<Map<String, Object>>>() {});
+        Assert.assertThat(genericResponse.get(0).get("table"), equalTo("A"));
     }
 
     @Step
     public void rateIsLowerThan(BigDecimal rateThreshold) {
-//        when().get(address).then().body("[0].rates.find {it.code='" + rateThreshold + "'}.mid", lessThan(0.13));
+        given().config(RestAssured.config().jsonConfig(jsonConfig().numberReturnType(BIG_DECIMAL)))
+                .when().get(address).then().body("[0].rates.find {it.code='" + rateThreshold + "'}.mid", lessThan(rateThreshold));
         Assert.assertThat(rate, lessThan(rateThreshold));
     }
 
