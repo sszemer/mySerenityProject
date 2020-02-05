54ec7afea05689c05feb187136cf8f89541bb177
sszemer <Sk@te123#>
2020-02-05 14:42:10 +0100

    zabawki

--- src/test/java/org/example/steps/serenity/RESTSteps.java
+++ src/test/java/org/example/steps/serenity/RESTSteps.java
@@ -1,7 +1,10 @@
 package org.example.steps.serenity;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.common.mapper.TypeRef;
+import io.restassured.listener.ResponseValidationFailureListener;
+import io.restassured.specification.ResponseSpecification;
 import net.thucydides.core.annotations.Step;
 import org.example.pojos.Nbp;
 import org.junit.Assert;
@@ -10,9 +13,11 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static io.restassured.config.FailureConfig.failureConfig;
 import static io.restassured.config.JsonConfig.jsonConfig;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static io.restassured.path.json.config.JsonPathConfig.NumberReturnType.BIG_DECIMAL;
@@ -21,18 +26,40 @@ import static org.hamcrest.Matchers.lessThan;
 
 public class RESTSteps {
 
-    String address = "http://api.nbp.pl/api/exchangerates/tables/A/?format=json";
+//    String address = "http://api.nbp.pl/api/exchangerates/tables/A/?format=json";
+    String address = "http://localhost:8080/api/exchangerates/tables/A/?format=json";
+    String proxy = "api.nbp.pl";
+    String mock = "localhost:8080";
     String schema = "REST/schemas/nbp.json";
     BigDecimal rate;
-    Nbp[] response;
+    ResponseValidationFailureListener emailOnFailure = (reqSpec, respSpec, resp) -> System.err.println("Important test failed! Status code was: " + resp.statusCode());
 
     @Step
     public void getCurrencyRateFor(String currency) {
-        given().get(address).then().assertThat().body(matchesJsonSchemaInClasspath(schema));
-        rate = given().config(RestAssured.config().jsonConfig(jsonConfig().numberReturnType(BIG_DECIMAL)))
-                .and().get(address)
-                .then().statusCode(200).extract().jsonPath().get("[0].rates.find {it.code='" + currency + "'}.mid");
+
+        ResponseSpecBuilder builder = new ResponseSpecBuilder();
+        builder.expectStatusCode(200);
+        builder.expectResponseTime(lessThan(1L), TimeUnit.SECONDS);
+        ResponseSpecification responseSpec = builder.build();
+
+        given()
+                .config(RestAssured.config().failureConfig(failureConfig().with().failureListeners(emailOnFailure)))
+                .get(address)
+            .then()
+                .spec(responseSpec)
+                .log().all()
+                .assertThat().body(matchesJsonSchemaInClasspath(schema));
+        rate = given()
+                    .config(RestAssured.config().jsonConfig(jsonConfig().numberReturnType(BIG_DECIMAL)))
+                .and()
+                    .get(address)
+                .then()
+                    .spec(responseSpec)
+                    .log().ifValidationFails()
+                .extract()
+                    .jsonPath().get("[0].rates.find {it.code='" + currency + "'}.mid");
 //        deserializacja
+        Nbp[] response;
         response = get(address).body().as(Nbp[].class);
         Objects.requireNonNull(response[0].getRates().stream().filter(rate1 -> rate1.getCode().equals(currency)).findAny().orElse(null)).getMid();
 //        deserializaca generyczna
@@ -42,8 +69,14 @@ public class RESTSteps {
 
     @Step
     public void rateIsLowerThan(BigDecimal rateThreshold) {
-        given().config(RestAssured.config().jsonConfig(jsonConfig().numberReturnType(BIG_DECIMAL)))
-                .when().get(address).then().body("[0].rates.find {it.code='" + rateThreshold + "'}.mid", lessThan(rateThreshold));
+        given()
+                .config(RestAssured.config().jsonConfig(jsonConfig().numberReturnType(BIG_DECIMAL)))
+        .when()
+                .get(address)
+        .then()
+                .log().ifError()
+                .body("[0].rates.find {it.code='" + rateThreshold + "'}.mid", lessThan(rateThreshold))
+                .time(lessThan(1L), TimeUnit.SECONDS); // to nie jest najwiarygodniejsze
         Assert.assertThat(rate, lessThan(rateThreshold));
     }
 
